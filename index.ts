0865c5918d070d0109af6ed6159e942db57eef90
AlishbaShahid02 <163669338+AlishbaShahid02@users.noreply.github.com>
2024-04-11 15:52:38 +0500

    number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"; 
+import chalk from "chalk";
+
+
+const randomNumber = Math.floor(Math.random()*10 );
+console.log(chalk.bold.magenta("Welcome to Number Guessing Game"));
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+      message: "Please Guess the number between 1-10"
+}
+]);
+if(answers.userGuessedNumber === randomNumber){
+console.log(chalk.cyan(`"Congratulations! You guessed right number"`));
+} else{
+console.log(chalk.red(`"You guessed wrong number"`));
+}
